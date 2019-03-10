190337cf7a98e542629d3f14985876478f3d607c
Kudo <kudo@example.com>
2019-03-10 09:07:51 +0900

    First commit

---
+++ fizzbuzz2.py
@@ -0,0 +1,17 @@
+# 1からNまで
+n = int(input("最大値を入力してね: "))
+
+nature_figure = range(1, n)
+
+# nature_figure = int(input("1つの自然数を入れてね: "))
+#
+# # print(nauture_figure)
+#
+# if nature_figure % 15 == 0:
+#     print("fizzbuzz")
+# elif nature_figure % 3 == 0:
+#     print("fizz")
+# elif nature_figure % 5 == 0:
+#     print("buzz")
+# else:
+#     print(nature_figure)
